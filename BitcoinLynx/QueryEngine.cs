38999c60a57126af96bdd8adf3502af155825740
bbulgu <bbulgudacity@gmail.com>
2023-06-06 13:58:24 +0200

    extract the get requests to its own class.

---
+++ BitcoinLynx/QueryEngine.cs
@@ -0,0 +1,60 @@
+﻿using BitcoinLynx.ApiParsers;
+using BitcoinLynx.Parser;
+
+
+namespace BitcoinLynx
+{
+    
+    public class QueryEngine
+    {
+        string url;              // url to get (depends on api)
+        public HttpClient client;
+
+        public QueryEngine(string url)
+        {
+            this.url = url;
+            client = new HttpClient();
+        }
+
+        int max_retries = 5;
+        int retry_delay = 1000;
+        int retries = 0;
+
+        // A simple engine to send get requests and return the json sent back from the server
+        // Try for max_retries amount of times
+        public async Task<string?> queryApi()
+        {
+            while (retries < max_retries)
+            {
+                try
+                {
+                    // Perform the GET request
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the response content
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                    // TODO: What else in the error handling?
+                    else
+                    {
+                        Console.WriteLine($"Request for url {url} failed with status code: {response.StatusCode}");
+                        retries++;
+                        Console.WriteLine($"Retrying for the {retries} time after a {retry_delay}ms delay. Will stop after {max_retries} times.");
+                        await Task.Delay(1000);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+            Console.WriteLine($"After {max_retries} retries, stopping.");
+            return null;
+        }
+    
+    }
+}
--- BitcoinLynx/TradeData.cs
+++ BitcoinLynx/TradeData.cs
@@ -16,15 +16,9 @@ namespace BitcoinLynx
         string currencypair;    // which currency pair to query for
 
         string timestamp;        // a unix time stamp: time to query from (to now)
-        string url;              // url to get (depends on api)
-        public HttpClient client;
         public List<Transaction> listOfTransactions = new List<Transaction>(); // List of transactions that took place from timespan to now
 
         public TradeStats tradeStats;
-
-        int max_retries = 5;
-        int retry_delay = 1000;
-        int retries = 0;
 
 
         public TradeData() // init with defaults
@@ -47,8 +41,6 @@ namespace BitcoinLynx
         private void initAttributes()
         {
             timestamp = calculateTimeStamp(this.mins_ago);
-            url = ApiRequestBuilder.getUrl(api, currencypair, timestamp);
-            client = new HttpClient();
             listOfTransactions = new List<Transaction>();
         }
 
@@ -63,38 +55,18 @@ namespace BitcoinLynx
         // method to query the chosen api with the chosen currency pair
         public async Task QueryTradeData()
         {
-            while (retries < max_retries)
+            QueryEngine engine = new QueryEngine(ApiRequestBuilder.getUrl(api, currencypair, timestamp));
+            string jsonString = await engine.queryApi();
+            
+            if (jsonString == null)
             {
-                try
-                {
-                    // Perform the GET request
-                    HttpResponseMessage response = await client.GetAsync(url);
-
-                    // Check if the request was successful
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Read the response content
-                        string jsonString = await response.Content.ReadAsStringAsync();
-                        ApiParser parser = ApiParserFactory.GetApiParser(api, timestamp);
-                        listOfTransactions = parser.processJsonString(jsonString);
-                        return;
-                    }
-                    // TODO: What else in the error handling?
-                    else
-                    {
-                        Console.WriteLine($"Request for api {api} failed with status code: {response.StatusCode}");
-                        retries++;
-                        Console.WriteLine($"Retrying for the {retries} time after a {retry_delay}ms delay. Will stop after {max_retries} times.");
-                        await Task.Delay(1000); 
-                    }
-
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An error occurred: " + ex.Message);
-                }
+                Console.WriteLine($"Request for api {api} failed.");
             }
+            else
+            {
+                ApiParser parser = ApiParserFactory.GetApiParser(api, timestamp);
+                listOfTransactions = parser.processJsonString(jsonString);
+            }                 
         }
         
     }
